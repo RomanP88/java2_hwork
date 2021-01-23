6d7bcee557104511e1ac01f2711e2f6598ba5fbe
RomanP88 <rasamaxa1905@gmail.com>
2021-01-23 18:45:19 +0300

    hw

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField textField;
+
+
+    @FXML
+    private TextArea textAria;
+
+    @FXML
+    public void clickBTNSend(ActionEvent actionEvent) {
+        textAria.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+    @FXML
+    public void clickBtn1(ActionEvent actionEvent) {
+        textAria.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
